50302b57ceec92f9957c9dac0b9935783281d51b
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-14 15:49:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -18,15 +18,15 @@ def help
     puts "- exit : exits this program"
 end 
 
+
+
 def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.strip
 end
 
 =begin 
-def list(songs)
-  songs.each_with_index 
-end 
+
 
 def exit_jukebox
 end
