976b9bf90dc673b078152a4608df27425e12954c
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-14 16:01:01 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,10 +22,6 @@ def list(songs)
   songs.each_with_index { |song, index| puts "#{(index + 1).to_s}. #{song}"}
 end 
 
-
-
-
-
 def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.strip
@@ -35,7 +31,7 @@ def exit_jukebox
   puts "Goodbye"
 end
 
-
+=begin 
 def run 
   puts "Please enter a command:"
   input = gets.strip
