ef8f897b0541fc9b5169862e2c205475fb6149bf
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-14 15:36:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,7 +10,7 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-input = gets.strip
+
 
 def help
   input = gets.strip
@@ -37,5 +37,6 @@ end
 
 def run 
   puts "Please enter a command:"
+  
 end 
 
