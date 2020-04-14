816cd36b5080f957eca8b5ef18163f44a51477cd
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-14 15:05:38 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,7 +14,7 @@ puts "Please enter a command:"
 command = gets.strip
 
 def help
-  if command == "help".downcase
+  if command.downcase == "help"
     puts "I accept the following commands:"
     puts "- help : displays this help message"
     puts "- list : displays a list of songs you can play"
