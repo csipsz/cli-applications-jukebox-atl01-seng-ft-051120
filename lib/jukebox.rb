8825a36f3cfa326972f416173d16a24f1f04ccac
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-14 15:00:12 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,7 +15,12 @@ puts "Please enter a command:"
 def help
   command = gets.strip
   if command == "help".downcase
-    
+    I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+  end
 end 
 
 def play 
