3708527dae4fa05013f6a6e088c310cae629ecf4
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-14 16:09:35 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,11 +27,10 @@ def play(songs)
   choice = gets.strip
   songs.each_with_index do |song, index| 
     if choice == song || choice == index + 1
-      puts "Playing #{song}"
-      
+      return "Playing #{song}"
     end
   end
-  
+  puts "Invalid input, please try again"
 end
 
 def exit_jukebox
